8ed81c8b0d7483d8585abff32d4f34ceb20a1842
daviddcer22 <davidcergall22@gmail.com>
2024-01-19 13:14:51 +0100

    Añadida auditorías de tablas Consulta y Contrato

--- my-app/src/main/java/es/uca/iw/auditoria/CustomAuditingEntityListener.java
+++ my-app/src/main/java/es/uca/iw/auditoria/CustomAuditingEntityListener.java
@@ -1,6 +1,8 @@
 package es.uca.iw.auditoria;
 
+import es.uca.iw.atencion_cliente.Consulta;
 import es.uca.iw.cliente.Cliente;
+import es.uca.iw.contrato.Contrato;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import java.time.LocalDateTime;
@@ -11,6 +13,10 @@ public class CustomAuditingEntityListener {
     public void prePersist(Object entity) {
         if (entity instanceof Cliente cliente) {
             cliente.setCreatedDate(LocalDateTime.now());
+        } else if (entity instanceof Contrato contrato) {
+            contrato.setCreatedDate(LocalDateTime.now());
+        } else if (entity instanceof Consulta consulta) {
+            consulta.setCreatedDate(LocalDateTime.now());
         }
     }
 
@@ -18,7 +24,12 @@ public class CustomAuditingEntityListener {
     public void preUpdate(Object entity) {
         if (entity instanceof Cliente cliente) {
             cliente.setLastModifiedDate(LocalDateTime.now());
+        } else if (entity instanceof Contrato contrato) {
+            contrato.setLastModifiedDate(LocalDateTime.now());
+        } else if (entity instanceof Consulta consulta) {
+            consulta.setLastModifiedDate(LocalDateTime.now());
         }
     }
 }
+
 
